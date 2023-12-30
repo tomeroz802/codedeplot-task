85ef76ef162469f767623c6feaaff098150e8db2
tomer oz <no@way.com>
2023-12-30 21:09:10 +0200

    niss 10

--- scripts/stop_nginx.sh
+++ scripts/stop_nginx.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-sudo docker ps -a | grep nginx-tomer
-EXITCODE=$?
-if [ "$EXITCODE" -ne "0" ]; then
-    #this is the catch part
-    echo "uh oh"
-    exit 0
-else
-    sudo docker stop nginx-tomer
-    sudo docker rm nginx-tomer
-
-fi
+sudo docker stop nginx-tomer
+sudo docker rm nginx-tomer
