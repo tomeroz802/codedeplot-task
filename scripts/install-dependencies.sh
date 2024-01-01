efb68fc0dc6e29e33b38ba70394aa0e7c447e2d3
tomer oz <no@way.com>
2024-01-01 20:52:15 +0200

    delete all

--- scripts/install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo yum update -y 
-sudo yum install docker -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -a -G docker $(whoami) 
-
-
-
--- scripts/install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#sudo yum install docker -y
-sudo curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl enable docker
