e6664fd98a5cf3ddb97eb7d43c5880f76281d2ac
tomer oz <no@way.com>
2024-01-01 19:59:12 +0200

    test 2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #sudo yum install docker -y
-curl -fsSL https://get.docker.com/ | sh
+sudo curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo systemctl enable docker
